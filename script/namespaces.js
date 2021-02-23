b6a8b3231628ecdbb15683f3ed913c9f1c1191fc
tomicnikola <50-2018@ftn.edu.rs>
2021-02-23 19:40:40 +0100

    Objects page fully created, minor adjustments to other pages.

--- script/namespaces.js
+++ script/namespaces.js
@@ -2,4 +2,18 @@ function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
     }
   
-  
+$(document).ready(function(){
+    var i = 0, text;
+    text = "Namespaces";
+    
+    function typing(){
+        if(i < text.length){
+            document.getElementById("nm").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing,120); 
+        }
+        
+    }
+    typing();
+    
+});
