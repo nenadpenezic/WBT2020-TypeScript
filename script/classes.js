fa495a9a888d817b12bcae1f8cdac9248f79d275
tomicnikola <50-2018@ftn.edu.rs>
2021-02-23 23:10:19 +0100

    Classes page fully created.

--- script/classes.js
+++ script/classes.js
@@ -1,3 +1,19 @@
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+
+$(document).ready(function(){
+    var i = 0, text;
+    text = "Classes";
+    
+    function typing(){
+        if(i < text.length){
+            document.getElementById("cs").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing,120); 
+        }
+        
     }
+    typing();
+    
+});
