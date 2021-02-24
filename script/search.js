22c235b9f49ae144f420063283b31453f9344e8b
nenadpenezic <nenad.penezi264@gmail.com>
2021-02-24 11:47:20 -0800

    Minior changes.

--- script/search.js
+++ script/search.js
@@ -1,5 +1,5 @@
 const pages=[{page:'arrays.html',name:'Arrays'},{page:'basicSyntax.html',name:'Basic Syntax'},{page:'classes.html',name:'Classes'},{page:'Documentation.html',name:'Documentation'},{page:'Functions.html',name:'Functions'},
-{page:'home.html',name:'Home'},{page:'Interfaces.html',name:'Interfaces'},{page:'Loops.html',name:'Loops'},{page:'modules&ambients.html',name:'Modules & Ambients'},{page:'namespaces.html',name:'Namespaces'},{pages:'objects.html',name:'Objects'},
+{page:'home.html',name:'Home'},{page:'Interfaces.html',name:'Interfaces'},{page:'Loops.html',name:'Loops'},{page:'modules&ambients.html',name:'Modules & Ambients'},{page:'namespaces.html',name:'Namespaces'},{page:'objects.html',name:'Objects'},
 {page:'Operators.html',name:'Operators'},{page:'Overview.html',name:'Overview'},{page:'Strings.html',name:'Strings'},{page:'Types.html',name:'Types'},{page:'u&itypes.html',name:'Union and Intersection Types'}];
 let search=document.getElementById('search');
 let printResults=document.getElementsByClassName('search-list')[0];
