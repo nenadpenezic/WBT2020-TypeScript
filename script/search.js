b796ec0098feb68bfad7a0c881c1895bcaf649bf
nenadpenezic <nenad.penezi264@gmail.com>
2021-02-24 04:50:10 -0800

    Final changes.

--- script/search.js
+++ script/search.js
@@ -1,6 +1,6 @@
-const pages=['arrays.html','basicSyntax.html','classes.html','Documentation.html','Functions.html',
-'home.html','Interfaces.html','Loops.html','modules&ambients.html','namespaces.html','objects.html',
-'Operators.html','Overview.html','Strings.html','Types.html','u&itypes.html'];
+const pages=[{page:'arrays.html',name:'Arrays'},{page:'basicSyntax.html',name:'Basic Syntax'},{page:'classes.html',name:'Classes'},{page:'Documentation.html',name:'Documentation'},{page:'Functions.html',name:'Functions'},
+{page:'home.html',name:'Home'},{page:'Interfaces.html',name:'Interfaces'},{page:'Loops.html',name:'Loops'},{page:'modules&ambients.html',name:'Modules & Ambients'},{page:'namespaces.html',name:'Namespaces'},{pages:'objects.html',name:'Objects'},
+{page:'Operators.html',name:'Operators'},{page:'Overview.html',name:'Overview'},{page:'Strings.html',name:'Strings'},{page:'Types.html',name:'Types'},{page:'u&itypes.html',name:'Union and Intersection Types'}];
 let search=document.getElementById('search');
 let printResults=document.getElementsByClassName('search-list')[0];
 function searchFunction(){
@@ -11,8 +11,8 @@ function searchFunction(){
     else
     printResults.style.display='block';
     for(let i=0;i<pages.length;i++){
-        if(pages[i].indexOf(searchVale)>-1){
-            searcResults+='<li><a href="./'+pages[i]+'">'+pages[i].slice(0,pages[i].length-5)+'</a></li>';
+        if(pages[i].name.indexOf(searchVale)>-1){
+            searcResults+='<li><a href="./'+pages[i].page+'">'+pages[i].name+'</a></li>';
         }
     }
     console.log(searcResults);
